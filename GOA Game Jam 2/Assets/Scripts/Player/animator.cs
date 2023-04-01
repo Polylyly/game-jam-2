aa65032aa4a05901b2ee67c857adadaa21b0c25a
Jimmyidds <123501633+Jimmyidds@users.noreply.github.com>
2023-04-01 15:24:05 -0400

    Player animations 2

--- GOA Game Jam 2/Assets/Scripts/Player/animator.cs
+++ GOA Game Jam 2/Assets/Scripts/Player/animator.cs
@@ -10,15 +10,21 @@ public class animator : MonoBehaviour
         {
             gameObject.GetComponent<Animator>().Play("Player-Dash");
         }
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
         {
             gameObject.GetComponent<Animator>().Play("Player-Walk");
+        }
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            gameObject.GetComponent<Animator>().Play("Player-Crouch");
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameObject.GetComponent<Animator>().Play("Player-Jump");
         }
         if (!Input.anyKey)
         {
             gameObject.GetComponent<Animator>().Play("Player-Idle");
         }
-
-
     }
 }
